f0b4095ad4a3dc9a6f0e6d979693a5c3b19c321f
simple_scout <vitale232@gmail.com>
2020-04-11 07:40:51 -0400

    Cleanup the log cleanup logs

--- source/utils/cleanup_logs.py
+++ source/utils/cleanup_logs.py
@@ -24,11 +24,14 @@ def main():
 
     nyt_logs_dir = os.path.join(logs_dir, 'nyt')
     peese_logs_dir = os.path.join(logs_dir, 'peese')
+    utils_logs_dir = os.path.join(logs_dir, 'utils')
 
     log_files = [
         os.path.join(nyt_logs_dir, file_) for file_ in os.listdir(nyt_logs_dir)
     ] + [
         os.path.join(peese_logs_dir, file_) for file_ in os.listdir(peese_logs_dir)
+    ] + [
+        os.path.join(utils_logs_dir, file_) for file_ in os.listdir(utils_logs_dir)
     ]
 
     print(f'\nScanning log dir for files older than {delete_delta}:\n Log Dir : {nyt_logs_dir}')
